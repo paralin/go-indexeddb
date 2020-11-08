18724501cce0eee1966325be565ef4d24c76b5e5
Christian Stewart <christian@paral.in>
2020-11-07 21:23:23 -0800

    fix: handle panics when calling lookup methods on objstore wasm
    Signed-off-by: Christian Stewart <christian@paral.in>

--- object_store_wasm.go
+++ object_store_wasm.go
@@ -17,7 +17,12 @@ func (s *ObjectStore) GetName() string {
 }
 
 // Put puts data into the store.
-func (s *ObjectStore) Put(value interface{}, key interface{}) error {
+func (s *ObjectStore) Put(value interface{}, key interface{}) (e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	value = MaybeConvertValueToJs(value)
 	key = MaybeConvertValueToJs(key)
 	_, err := WaitRequest(s.val.Call("put", value, key))
@@ -25,7 +30,12 @@ func (s *ObjectStore) Put(value interface{}, key interface{}) error {
 }
 
 // Add adds data to the store.
-func (s *ObjectStore) Add(value interface{}, key interface{}) error {
+func (s *ObjectStore) Add(value interface{}, key interface{}) (e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	value = MaybeConvertValueToJs(value)
 	key = MaybeConvertValueToJs(key)
 	_, err := WaitRequest(s.val.Call("add", value, key))
@@ -33,44 +43,74 @@ func (s *ObjectStore) Add(value interface{}, key interface{}) error {
 }
 
 // Delete deletes data from the store.
-func (s *ObjectStore) Delete(query interface{}) error {
+func (s *ObjectStore) Delete(query interface{}) (e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	query = MaybeConvertValueToJs(query)
 	_, err := WaitRequest(s.val.Call("delete", query))
 	return err
 }
 
 // Clear clears all data from the store.
-func (s *ObjectStore) Clear() error {
+func (s *ObjectStore) Clear() (e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	_, err := WaitRequest(s.val.Call("clear"))
 	return err
 }
 
 // Get gets data from the store
-func (s *ObjectStore) Get(query interface{}) (js.Value, error) {
+func (s *ObjectStore) Get(query interface{}) (rv js.Value, e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	query = MaybeConvertValueToJs(query)
 	return WaitRequest(s.val.Call("get", query))
 }
 
 // GetKey gets data from the store by key.
-func (s *ObjectStore) GetKey(query interface{}) (js.Value, error) {
+func (s *ObjectStore) GetKey(query interface{}) (_ js.Value, e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	query = MaybeConvertValueToJs(query)
 	return WaitRequest(s.val.Call("getKey", query))
 }
 
 // GetAll gets all values matching an optional query with an optional count.
-func (s *ObjectStore) GetAll(query interface{}) (js.Value, error) {
+func (s *ObjectStore) GetAll(query interface{}) (_ js.Value, e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	query = MaybeConvertValueToJs(query)
 	return WaitRequest(s.val.Call("getAll", query))
 }
 
 // GetAllKeys gets all keys matching an optional query with an optional count.
-func (s *ObjectStore) GetAllKeys(query interface{}) (js.Value, error) {
+func (s *ObjectStore) GetAllKeys(query interface{}) (_ js.Value, e error) {
 	query = MaybeConvertValueToJs(query)
 	return WaitRequest(s.val.Call("getAllKeys", query))
 }
 
 // Count counts keys matching the optional query.
-func (s *ObjectStore) Count(query interface{}) (int, error) {
+func (s *ObjectStore) Count(query interface{}) (_ int, e error) {
+	defer func() {
+		if err := recover(); err != nil {
+			e, _ = err.(error)
+		}
+	}()
 	query = MaybeConvertValueToJs(query)
 	v, err := WaitRequest(s.val.Call("count", query))
 	if err != nil {
